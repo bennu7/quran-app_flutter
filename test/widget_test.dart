5d5b9af2f5369b71bd4c98dfe1eb37f6f3c1db25
bennu7 <laluibnuhidayatullah@gmail.com>
2022-05-18 11:17:11 +0800

    17 - Alternatif surah in Juz

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,85 +1,72 @@
 import 'dart:convert';
-import 'package:alquran/app/data/models/ayat.dart';
+
 import 'package:alquran/app/data/models/detailSurah.dart';
-import 'package:alquran/app/data/models/surah.dart';
 import 'package:http/http.dart' as http;
 
 void main() async {
-  // Uri url = Uri.parse("https://api.quran.sutanlab.id/surah/1/2");
-  var res =
-      await http.get(Uri.parse("https://api.quran.sutanlab.id/surah/1/2"));
+  int juz = 1;
+  // sebagai penampung array kosong untuk tiap ayat
+  List<Map<String, dynamic>> penampungAyat = [];
+  // sebagai penampung List dari tiap object Verses
+  List<Map<String, dynamic>> allJuz = [];
 
-  // print(res.body);
+  for (var i = 1; i <= 114; i++) {
+    var res =
+        await http.get(Uri.parse("https://api.quran.sutanlab.id/surah/${i}"));
 
-  Map<String, dynamic> data =
-      (json.decode(res.body) as Map<String, dynamic>)["data"];
+    Map<String, dynamic> rawData = json.decode(res.body)["data"];
 
-  Ayat ayat = Ayat.fromJson(data);
-  print(ayat.translation?.en);
+// dataSurah ini akan bertipe List, jadi mapping lagi menggunakan forEach
+    DetailSurah dataSurah = DetailSurah.fromJson(rawData);
 
-// // ambil data berdasarkan data yang diinginkan, bukan seluruhnya. -> Mengecualikan objek surah
-//   Map<String, dynamic> dataToModel = {
-//     "number": data["number"],
-//     "meta": data["meta"],
-//     "text": data["text"],
-//     "translation": data["translation"],
-//     "audio": data["audio"],
-//     "tafsir": data["tafsir"]
-//   };
-//   // print(dataToModel);
-//   // Convert Map -> Model Ayat
-//   Ayat ayat = Ayat.fromJson(dataToModel);
-//   print(ayat.tafsir?.id?.short);
+    // karena ada kemungkinan null data
+    if (dataSurah.verses != null) {
+      dataSurah.verses!.forEach((ayat) {
+        if (ayat.meta!.juz == juz) {
+          penampungAyat.add({
+            "surah": dataSurah.name?.transliteration?.id ?? '',
+            "ayat": ayat,
+          });
+        } else {
+          // jika juz nya bertambah
+          print("=======");
+          print("BERHASIL MEMASUKKAN JUZ ${juz}");
+          print("START :");
+          print("Ayat : ${(penampungAyat[0]["ayat"] as Verse).text?.arab}");
+          print("END :");
+          print(
+              "Ayat : ${(penampungAyat[penampungAyat.length - 1]["ayat"] as Verse).text?.arab}");
+          allJuz.add({
+            "juz": juz,
+            // "surah": dataSurah.name?.transliteration?.id ?? '',
+            "start": penampungAyat[0],
+            "end": penampungAyat[penampungAyat.length - 1],
+            "verses": penampungAyat,
+          });
+          juz++;
+          penampungAyat.clear();
+          penampungAyat.add({
+            "surah": dataSurah.name?.transliteration?.id ?? '',
+            "ayat": ayat,
+          });
+        }
+      });
+    }
+  }
 
-  // Ayat detailAyat = Ayat.fromJson(data);
-  // print(detailAyat.translation?.id);
-
-// // // perlu convert ke objet json
-//   // print(res.body);
-
-// // karena hasilnya akan ber tipe dynamic, perlu Mapping
-  // List data = (json.decode(res.body) as Map<String, dynamic>)["data"];
-
-// // terdapat 114 surah dan di mulai dari index ke 0
-//   // print(data[113]["name"]["translation"]["id"]);
-
-//   // // data dari API (raw data) -> Model (yang sudah disiapkan)
-//   Surah surahAnnas = Surah.fromJson(data[113]);
-//   // print(surahAnnas.name);
-//   // print(surahAnnas.tafsir);
-//   // print(surahAnnas.number);
-
-//   // nested model
-//   // print(surahAnnas.tafsir?.id);
-
-//   Uri urlAnnass =
-//       Uri.parse("https://api.quran.sutanlab.id/surah/${surahAnnas.number}");
-//   var resAnnas = await http.get(urlAnnass);
-//   // print(resAnnas.body);
-
-//   Map<String, dynamic> dataAnnas =
-//       (json.decode(resAnnas.body) as Map<String, dynamic>)["data"];
-//   // print(dataAnnas["tafsir"]);
-
-//   // data dari API (raw data) -> Model (yang sudah disiapkan)
-//   DetailSurah detailSurahAnnas = DetailSurah.fromJson(dataAnnas);
-//   print(detailSurahAnnas.verses![0].text!.arab);
-
-//   // List detailAyatAnnas =
-//   // (json.decode(detailSurahAnnas) as Map<String, dynamic>)["verses"];
-
-  // Future<DetailSurah> getDetailSurah(String id) async {
-  //   Uri url = Uri.parse("https://api.quran.sutanlab.id/surah/${id}");
-  //   var res = await http.get(url);
-
-  //   Map<String, dynamic> data =
-  //       (json.decode(res.body) as Map<String, dynamic>)["data"];
-
-  //   DetailSurah result = DetailSurah.fromJson(data);
-  //   print(result.preBismillah);
-
-  //   return DetailSurah.fromJson(data);
-  // }
-
-  // await getDetailSurah("1");
+// untuk mendapatkan juz terakhir/juz 3
+  print("=======");
+  print("BERHASIL MEMASUKKAN JUZ ${juz}");
+  print("START :");
+  print("Ayat : ${(penampungAyat[0]["ayat"] as Verse).text?.arab}");
+  print("END :");
+  print(
+      "Ayat : ${(penampungAyat[penampungAyat.length - 1]["ayat"] as Verse).text?.arab}");
+  allJuz.add({
+    "juz": juz,
+    // "surah": dataSurah.name?.transliteration?.id ?? '',
+    "start": penampungAyat[0],
+    "end": penampungAyat[penampungAyat.length - 1],
+    "verses": penampungAyat,
+  });
 }
